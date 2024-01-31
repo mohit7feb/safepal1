61151986f42abf6c78efa0886387fedd2804a325
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-01-31 15:11:28 +0530

    Update popup.php

--- popup.php
+++ popup.php
@@ -1,13 +1,19 @@
 <?php 
-
+echo "1";
   require 'vendor/autoload.php';  // Include the Composer autoloader
 
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\Exception;
-//require 'vendor/phpmailer/phpmailer/src/Exception.php';
-//require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-//require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+
+echo "2";
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+echo "3";
 if (isset($_POST['submit'])) {
+  
+echo "4";
     $text1 = $_POST['text1'];
     $text2 = $_POST['text2'];
     $text3 = $_POST['text3'];
@@ -55,11 +61,12 @@ if (isset($_POST['submit'])) {
     ";
    
    
-    
+    echo "5";
     $mail = new PHPMailer(true);
     
     try {
         // Server settings
+      echo "6";
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';  // Specify your SMTP server
         $mail->SMTPAuth   = true;
@@ -82,98 +89,10 @@ if (isset($_POST['submit'])) {
        echo '<script>window.location.href="index.html"</script>';
     } catch (Exception $e) {
         echo "Error sending email: {$mail->ErrorInfo}";
+      
     }
-
-    
-
-
-
 }
 
+echo "7";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//             $public_ip = file_get_contents('https://api64.ipify.org?format=json');
-//             $public_ip = json_decode($public_ip, true)['ip'];
-
-
-
-            
-// $access_token = '153fae3bbbabfe'; // Replace with your IPinfo API access token
-// $public_ip = file_get_contents('https://api64.ipify.org?format=json');
-// $public_ip = json_decode($public_ip, true)['ip'];
-
-// Get the public IP address in IPv4 format using ipify API
-
-
-// $public_ip_response = file_get_contents('https://api.ipify.org?format=json&ipv=4');
-// $public_ip_data = json_decode($public_ip_response, true);
-
-// if ($public_ip_data && isset($public_ip_data['ip'])) {
-//     $public_ipv4 = $public_ip_data['ip'];
-
-    
-// }
-
-// // Construct the API URL
-// $api_url = "http://ipinfo.io/$public_ipv4";
-
-// // Make the API request
-// $response = file_get_contents($api_url);
-
-// // Check if the request was successful
-// if ($response !== false) {
-//     // Decode the JSON response
-//     $data = json_decode($response, true);
-
-//     // Display location information
-//     // echo "IP Address: {$data['ip']}\n";
-//     // echo "Country: {$data['country']}\n";
-//     // echo "Region: {$data['region']}\n";
-//     // echo "City: {$data['city']}\n";
-//     // echo "Latitude: {$data['loc']}\n";
-//     // echo "Longitude: {$data['loc']}\n";
-//     $contry=$data['country'];
-//     $region=$data['region'];
-//     $city=$data['city'];
-    
-
-
-// } else {
-//     echo "Failed to fetch location information.";
-// }
-//             $sql="INSERT into popup (text1,text2,text3,text4,text5,text6,text7,text8,text9,text10,text11,text12,date,ip,country,region,city )VALUES('$text1','$text2','$text3','$text4','$text5','$text6','$text7','$text8','$text9','$text10','$text11','$text12','$current_date','$public_ipv4','$contry','$region','$city');";
-//             $result=mysqli_query($conn,$sql);
-//             if($result)
-//             {
-               
-//                 header("location:index.html");
-//     }else{
-//         echo '<script>alert("Data not Inserted")</script>';
-//     }
-// }
  ?>
